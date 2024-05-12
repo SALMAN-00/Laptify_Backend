92e210cc2d08b51a41b83f4fead79648e21de7d3
iplmr <45397969+iplmr@users.noreply.github.com>
2024-05-12 18:04:43 +0300

    Adding Origin CORS update Program.cs and DataBaseContext.cs

--- src/DataBases/DatabaseContext.cs
+++ src/DataBases/DatabaseContext.cs
@@ -11,22 +11,9 @@ namespace sda_onsite_2_csharp_backend_teamwork_The_countryside_developers
 
         public DbSet<Category> Category { get; set; }
         public DbSet<Payment> Payment { get; set; }
-        private IConfiguration _config;
-        public DatabaseContext(IConfiguration config)
-        {
-            _config = config;
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options){}
 
 
-            var dataSourceBuilder = new NpgsqlDataSourceBuilder(@$"Host={_config["Db:Host"]};
-            Username={_config["Db:Username"]};Database={_config["Db:Database"]};Password={_config["Db:Password"]}");
-            dataSourceBuilder.MapEnum<Role>();
-            var dataSource = dataSourceBuilder.Build();
-            optionsBuilder.UseNpgsql(dataSource).UseSnakeCaseNamingConvention();
-
-        }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
